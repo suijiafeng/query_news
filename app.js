a36e7584c24f412e0984bb1ee3bfa861dd70898b
suijiafeng <suijiafeng@hotmail.com>
2022-10-17 01:25:02 +0800

    feat: 添加 定时任务

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const juejin = require('./lib/juejin')
 const segmentfault = require('./lib/segmentfault')
 const sendMail= require('./lib/mailer')
 const dayjs = require('dayjs')
-
+const nodeCron = require("node-cron")
 
 async function main(){
   const content1 = await juejin()
   const content2 = await segmentfault()
   const content = ['<b>juejin推荐:</b>',...content1,'<br/><b>segmentfault推荐:</b>',...content2].join('<br/>')
-  const date = dayjs().format('YYYY/MM/DD')
+  const date = dayjs().format('YYYY/MM/DD HH:mm:ss')
   sendMail(date,content)
 }
-main()
+
+// https://github.com/node-cron/node-cron 
+nodeCron.schedule('0 10 * * *', () => {
+  // 这里cron是每天上午10点整执行一次
+  main()
+  console.log('running a task every minute');
+});
