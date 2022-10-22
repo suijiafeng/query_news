634958aaf6f4e992633618ee3711e8e88e064d31
suijiafeng <suijiafeng@hotmail.com>
2022-10-22 19:55:30 +0800

    refactor: 使用 Promise.all 简化原有代码

--- app.js
+++ app.js
@@ -4,11 +4,8 @@ const dayjs = require('dayjs')
 const nodeCron = require("node-cron")
 
 const run = (e) => {
-  browser.run().then(info => {
-    const content = Object.entries(info).flat(Infinity).map(item => {
-      if (item.indexOf('<') > -1) return item
-      return `<br/><b>${item} 推荐：</b>`
-    })
+  browser.run().then(arr => {
+    const content = arr.flat(Infinity)
     const date = dayjs().format('YYYY/MM/DD HH:mm:ss')
     console.log(`running at ${date}`);
     sendMail(date, content.join('<br/>'))
